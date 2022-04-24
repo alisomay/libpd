deda441e6abe41ec17c65b5f32d7b7ded1b10098
Ali Somay <alisomay@runbox.com>
2022-04-24 22:14:39 +0200

    Add messages to externals setup

--- libpd_wrapper/libpd_rs_bundled/src/libpd_rs_bundled.c
+++ libpd_wrapper/libpd_rs_bundled/src/libpd_rs_bundled.c
@@ -1,6 +1,7 @@
 #ifndef LIBPD_RS_BUNDLED
 #define LIBPD_RS_BUNDLED
 
+#include "m_pd.h"
 #include "../header/libpd_rs_bundled.h"
 #include <stdio.h>
 
@@ -11,11 +12,14 @@ void moog_tilde_setup();
 
 void libpd_rs_bundled_setup()
 {
-    printf("SETTING UP!");
+    post("Setting up bundled externals with libpd-rs..");
     // Externals
     freeverb_tilde_setup();
+    post("freeverb~ -> ok.");
     moog_tilde_setup();
+    post("moog~ -> ok.");
     //
+    post("All bundled externals successfully set up!");
 }
 
 #endif
